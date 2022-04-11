63ab76909527ca9890eac647b79796821be08a4a
ShraddhyaMaji <shraddhya.maji@aipxperts.com>
2022-04-11 14:55:58 +0530

    migration files added

---
+++ database/migrations/2022_04_11_080437_create_clients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->integer('lead_id')->index('lead_id');
+            $table->string('client_name,100');
+            $table->string('client_email,120');
+            $table->longText('client_other_details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
