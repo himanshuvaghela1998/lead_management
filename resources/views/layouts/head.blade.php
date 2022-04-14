845bec9dfed15d334bc463276431cbe067108156
ShraddhyaMaji <shraddhya.maji@aipxperts.com>
2022-04-14 15:34:00 +0530

    custom.css inclue in head

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -12,4 +12,5 @@
     <link href="{{asset('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{asset('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
     <!--end::Global Stylesheets Bundle-->
+    <link href="{{ asset('assets/css/custom.css')}}" rel="stylesheet" type="text/css"/>
 </head>
