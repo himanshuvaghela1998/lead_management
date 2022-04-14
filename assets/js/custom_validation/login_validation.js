13ea9307745537ca36087eeeaee927316d97ff17
himanshuvaghela1998 <himanshuvaghela248@gmail.com>
2022-04-14 15:02:18 +0530

    working on new theme

---
+++ assets/js/custom_validation/login_validation.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#login_form').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+            },
+        },
+        message: {
+            email: {
+                required: "Enter your Email",
+                email: "Please enter a valid email address.",
+            },
+            password: "Password is required",
+        },
+        submitHandler: function(form){
+            form.submit();
+        }
+    });
+});
