5e057e8aabed81ee49cfe6d595ace5168e07b9da
himanshuvaghela1998 <himanshuvaghela248@gmail.com>
2022-04-15 11:57:14 +0530

    fix layout

--- resources/views/layouts/foot.blade.php
+++ resources/views/layouts/foot.blade.php
@@ -1,24 +1,24 @@
-<script src="assets/plugins/global/plugins.bundle.js"></script>
-<script src="{{ asset('assets/plugins/global/plugins.bundle.js')}}"></script>
-<script src="{{ asset('assets/js/scripts.bundle.js')}}"></script>
+<script src="public/assets/plugins/global/plugins.bundle.js"></script>
+<script src="{{ asset('public/assets/plugins/global/plugins.bundle.js')}}"></script>
+<script src="{{ asset('public/assets/js/scripts.bundle.js')}}"></script>
 <!--end::Global Javascript Bundle-->
 <!--begin::Page Vendors Javascript(used by this page)-->
-<script src="{{ asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.js')}}"></script>
+<script src="{{ asset('public/assets/plugins/custom/fullcalendar/fullcalendar.bundle.js')}}"></script>
 <!--end::Page Vendors Javascript-->
 <!--begin::Page Custom Javascript(used by this page)-->
-<script src="{{ asset('assets/js/custom/widgets.js')}}"></script>
-<script src="{{ asset('assets/js/custom/apps/chat/chat.js')}}"></script>
-<script src="{{ asset('assets/js/custom/modals/create-app.js')}}"></script>
-<script src="{{ asset('assets/js/custom/modals/upgrade-plan.js')}}"></script>
-<script src="{{ asset('assets/js/jquery-validation/js/jquery.validate.min.js')}}" type="text/javascript"></script>
-<script src="{{asset('assets/js/jquery-validation/js/additional-methods.min.js')}}" type="text/javascript"></script>
-<script src="{{ asset('assets/js/custom/custom.js')}}"></script>
+<script src="{{ asset('public/assets/js/custom/widgets.js')}}"></script>
+<script src="{{ asset('public/assets/js/custom/apps/chat/chat.js')}}"></script>
+<script src="{{ asset('public/assets/js/custom/modals/create-app.js')}}"></script>
+<script src="{{ asset('public/assets/js/custom/modals/upgrade-plan.js')}}"></script>
+<script src="{{ asset('public/assets/js/jquery-validation/js/jquery.validate.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('public/assets/js/jquery-validation/js/additional-methods.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset('public/assets/js/custom/custom.js')}}"></script>
 <!--end::Page Custom Javascript-->
 <!-- Start Lead Listing -->
-<script src="{{ asset('assets/js/custom/custom.js') }}"></script>
+<script src="{{ asset('public/assets/js/custom/custom.js') }}"></script>
 <!-- End Lead Listing -->
  <!-- strat Toastr -->
- <script src="{{ asset('assets/js/toastr.js') }}"></script>
+ <script src="{{ asset('public/assets/js/toastr.js') }}"></script>
  <script>
   @if(Session::has('message'))
   toastr.options =
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,16 +1,16 @@
 <head>
     <base href="">
     <title>{{env('APP_NAME')}}</title>
-    <link rel="shortcut icon" href="{{asset('assets/media/logos/favicon.ico')}}" />
+    <link rel="shortcut icon" href="{{asset('public/assets/media/logos/favicon.ico')}}" />
     <!--begin::Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <!--end::Fonts-->
     <!--begin::Page Vendor Stylesheets(used by this page)-->
-    <link href="{{asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('public/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css')}}" rel="stylesheet" type="text/css" />
     <!--end::Page Vendor Stylesheets-->
     <!--begin::Global Stylesheets Bundle(used by all pages)-->
-    <link href="{{asset('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('public/assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('public/assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
     <!--end::Global Stylesheets Bundle-->
-    <link href="{{ asset('assets/css/custom.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('public/assets/css/custom.css')}}" rel="stylesheet" type="text/css"/>
 </head>
