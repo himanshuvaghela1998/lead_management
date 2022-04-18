551c92e1e7da96a751fcbca6d092079460b087c7
himanshuvaghela1998 <himanshuvaghela248@gmail.com>
2022-04-18 15:09:14 +0530

    list done

--- resources/views/leads/compact/lead_list.blade.php
+++ resources/views/leads/compact/lead_list.blade.php
@@ -14,7 +14,7 @@
     <tbody class="fw-bold text-gray-600">
         @foreach ($leads as $lead)
         <tr id="user_{{$lead->secret}}">
-            <td>{{ $lead->project_title }}</td>
+            <td> <p class="capitalize-letter">{{ $lead->project_title }} </p></td>
             <td> <p class="capitalize-letter">{{ str_replace('_', ' ', $lead->status) }} </p></td>
             <td> <p class="capitalize-letter">{{ ($lead->ProjectType) ? $lead->ProjectType->project_type : '' }} </p></td>
             <td> <p class="capitalize-letter">{{ ($lead->getUser) ? $lead->getUser->name : ''  }}</p></td>
