03f4d690294c450e4733a8f5db6c011269020540
himanshuvaghela1998 <himanshuvaghela248@gmail.com>
2022-05-06 13:23:16 +0530

    login error change done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,6 +51,16 @@
 							<form class="form w-100" method="post" action="{{ route('login') }}" id="login_form">
 								@csrf
                                 <!--begin::Heading-->
+                                @if($errors->any())
+                                <div class="alert alert-danger">
+                                    <p><strong>Opps Something went wrong</strong></p>
+                                    <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                    </ul>
+                                </div>
+                                @endif
                                 @if(session('error'))
                                 <div class="alert alert-danger">{{session('error')}}</div>
                                 @endif
@@ -85,16 +95,7 @@
 
 									<!--end::Input-->
 								</div>
-                                @if($errors->any())
-                                    <div class="alert alert-danger">
-                                        <p><strong>Opps Something went wrong</strong></p>
-                                        <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                        </ul>
-                                    </div>
-                                @endif
+
 
 
 
