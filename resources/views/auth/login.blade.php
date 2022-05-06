793b123630e29da591bbf579e6ad9c112cba2654
himanshuvaghela1998 <himanshuvaghela248@gmail.com>
2022-05-06 13:32:22 +0530

    changes done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,16 +51,6 @@
 							<form class="form w-100" method="post" action="{{ route('login') }}" id="login_form">
 								@csrf
                                 <!--begin::Heading-->
-                                @if($errors->any())
-                                <div class="alert alert-danger">
-                                    <p><strong>Opps Something went wrong</strong></p>
-                                    <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                    </ul>
-                                </div>
-                                @endif
                                 @if(session('error'))
                                 <div class="alert alert-danger">{{session('error')}}</div>
                                 @endif
